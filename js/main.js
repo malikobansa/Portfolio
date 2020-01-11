8e5cd92ad734413cdc8e99eb2073fd4d93ff351c
Muhammad Abdulmalik Obansa <56732809+muhammadobansa@users.noreply.github.com>
2020-01-11 08:48:00 +0100

    new stuff

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//Slider
+
+ function openSlideMenu(){
+     document.getElementById('side-menu').style.width = '250px';
+
+     document.getElementsByClassName('container').style.marginLeft = '250px';
+ }
+ function closeSlideMenu(){
+    document.getElementById('side-menu').style.width = '0px';
+
+    document.getElementsByClassName('container').style.marginLeft = '0px';
+}
+
+//Dark & Light Mode
+
